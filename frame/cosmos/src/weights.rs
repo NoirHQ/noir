2b55a1b87ee64c6a5d467eaa7c712326c92f4735
code0xff <ian.jungyong.um@gmail.com>
2024-09-11 20:23:08 +0900

    feat: Add pallet-cosmos weights

---
+++ frame/cosmos/src/weights.rs
@@ -0,0 +1,34 @@
+// This file is part of Noir.
+
+// Copyright (c) Haderech Pte. Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use core::marker::PhantomData;
+use frame_support::{dispatch::DispatchClass, weights::Weight};
+use sp_core::Get;
+
+pub trait WeightInfo {
+	fn default_weight() -> Weight;
+}
+
+pub struct CosmosWeight<T>(PhantomData<T>);
+impl<T> WeightInfo for CosmosWeight<T>
+where
+	T: frame_system::Config,
+{
+	fn default_weight() -> Weight {
+		T::BlockWeights::get().get(DispatchClass::Normal).base_extrinsic
+	}
+}
