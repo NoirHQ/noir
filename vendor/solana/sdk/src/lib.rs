32c8711ff9746270cc231b23b3a522150b077c62
code0xff <ian.jungyong.um@gmail.com>
2024-11-19 14:56:01 +0900

    feat: Activate additional modules in solana-program

--- vendor/solana/sdk/src/lib.rs
+++ vendor/solana/sdk/src/lib.rs
@@ -63,11 +63,11 @@ extern crate self as solana_sdk;
 //};
 pub use solana_program::{
     account_info, blake3, borsh1, bpf_loader, bpf_loader_deprecated, bpf_loader_upgradeable, clock,
-    decode_error, ed25519_program, epoch_schedule, fee_calculator, hash, incinerator, instruction,
-    keccak, lamports, loader_upgradeable_instruction, loader_v4, loader_v4_instruction, message,
-    native_token, nonce, program, program_stubs, rent, sanitize, secp256k1_program,
-    secp256k1_recover, short_vec, slot_history, stable_layout, syscalls, system_instruction,
-    system_program, sysvar,
+    decode_error, ed25519_program, epoch_rewards, epoch_schedule, fee_calculator, hash,
+    incinerator, instruction, keccak, lamports, loader_upgradeable_instruction, loader_v4,
+    loader_v4_instruction, message, native_token, nonce, program, program_error, program_stubs,
+    rent, sanitize, secp256k1_program, secp256k1_recover, short_vec, slot_hashes, slot_history,
+    stable_layout, stake_history, syscalls, system_instruction, system_program, sysvar,
 };
 
 pub mod account;
