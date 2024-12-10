88b2b695f394fe85a939175a1ec8df00d11d200e
Jeeyong Um <conr2d@proton.me>
2024-12-10 10:59:34 +0900

    feat: Add no_std support to solana-compute-budget

--- vendor/solana/compute-budget/src/lib.rs
+++ vendor/solana/compute-budget/src/lib.rs
@@ -1,4 +1,6 @@
 //! Solana compute budget types and default configurations.
+#![allow(unexpected_cfgs)]
+#![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(RUSTC_WITH_SPECIALIZATION, feature(min_specialization))]
 
 pub mod compute_budget;
