1382e1ff66aa39741b381d509619ed3866cdaec1
Jeeyong Um <conr2d@proton.me>
2024-12-10 10:36:03 +0900

    chore(deps): Vendor solana program runtime

---
+++ vendor/solana/program-runtime/src/lib.rs
@@ -0,0 +1,18 @@
+#![cfg_attr(RUSTC_WITH_SPECIALIZATION, feature(min_specialization))]
+#![deny(clippy::arithmetic_side_effects)]
+#![deny(clippy::indexing_slicing)]
+
+#[macro_use]
+extern crate eager;
+
+#[macro_use]
+extern crate solana_metrics;
+
+pub use solana_rbpf;
+pub mod invoke_context;
+pub mod loaded_programs;
+pub mod log_collector;
+pub mod mem_pool;
+pub mod stable_log;
+pub mod sysvar_cache;
+pub mod timings;
