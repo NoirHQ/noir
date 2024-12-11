cb80e92341dbb909946e5c78ea0bf336846054a5
code0xff <ian.jungyong.um@gmail.com>
2024-12-11 14:03:41 +0900

    feat: Add solana runtime apis

---
+++ primitives/solana/src/commitment_config.rs
@@ -0,0 +1,50 @@
+// This file is part of Noir.
+
+// Copyright (c) Haderech Pte. Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use parity_scale_codec::{Decode, Encode};
+use scale_info::TypeInfo;
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub struct CommitmentConfig {
+	pub commitment: CommitmentLevel,
+}
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+/// An attribute of a slot. It describes how finalized a block is at some point in time. For
+/// example, a slot is said to be at the max level immediately after the cluster recognizes the
+/// block at that slot as finalized. When querying the ledger state, use lower levels of commitment
+/// to report progress and higher levels to ensure state changes will not be rolled back.
+pub enum CommitmentLevel {
+	/// The highest slot of the heaviest fork processed by the node. Ledger state at this slot is
+	/// not derived from a confirmed or finalized block, but if multiple forks are present, is from
+	/// the fork the validator believes is most likely to finalize.
+	Processed,
+
+	/// The highest slot that has been voted on by supermajority of the cluster, ie. is confirmed.
+	/// Confirmation incorporates votes from gossip and replay. It does not count votes on
+	/// descendants of a block, only direct votes on that block, and upholds "optimistic
+	/// confirmation" guarantees in release 1.3 and onwards.
+	Confirmed,
+
+	/// The highest slot having reached max vote lockout, as recognized by a supermajority of the
+	/// cluster.
+	Finalized,
+}
---
+++ primitives/solana/src/response.rs
@@ -0,0 +1,107 @@
+// This file is part of Noir.
+
+// Copyright (c) Haderech Pte. Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::{
+	account::UiAccount,
+	clock::{Epoch, Slot},
+	transaction_error::TransactionError,
+};
+use alloc::string::String;
+use parity_scale_codec::{Decode, Encode};
+use scale_info::TypeInfo;
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub struct Response<T> {
+	pub context: ResponseContext,
+	pub value: T,
+}
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub struct ResponseContext {
+	pub slot: Slot,
+}
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub struct Blockhash {
+	pub blockhash: String,
+	pub last_valid_block_height: u64,
+}
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub struct SimulateTransactionResult {
+	pub err: Option<TransactionError>,
+	pub logs: Option<Vec<String>>,
+	pub accounts: Option<Vec<Option<UiAccount>>>,
+	pub units_consumed: Option<u64>,
+	pub return_data: Option<UiTransactionReturnData>,
+	pub inner_instructions: Option<Vec<UiInnerInstructions>>,
+	pub replacement_blockhash: Option<Blockhash>,
+}
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub struct UiTransactionReturnData {
+	pub program_id: String,
+	pub data: (String, UiReturnDataEncoding),
+}
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub enum UiReturnDataEncoding {
+	Base64,
+}
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub struct UiInnerInstructions {
+	/// Transaction instruction index
+	pub index: u8,
+	/// List of inner instructions
+	pub instructions: Vec<UiInstruction>,
+}
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub enum UiInstruction {
+	Compiled(UiCompiledInstruction),
+	// Parsed(UiParsedInstruction),
+}
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub struct UiCompiledInstruction {
+	pub program_id_index: u8,
+	pub accounts: Vec<u8>,
+	pub data: String,
+	pub stack_height: Option<u32>,
+}
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub struct InflationReward {
+	pub epoch: Epoch,
+	pub effective_slot: Slot,
+	pub amount: u64,            // lamports
+	pub post_balance: u64,      // lamports
+	pub commission: Option<u8>, // Vote account commission when the reward was credited
+}
